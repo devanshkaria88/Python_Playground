c0ed8cbac9f0a9965c54119b2851cc01f48df23c
devanshkaria88 <devansh88karia@gmail.com>
2020-02-06 21:36:43 +0530

    Added open file button

--- Tkinter Experiments/Experiment1.py
+++ Tkinter Experiments/Experiment1.py
@@ -3,6 +3,27 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
+
+def addApp():
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir='/', title='Select Files',
+                                          filetypes=(('Exetuables', '*.exe'), ('All Files', '*.*')))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg='grey')
+        label.pack()
+    runapps()
+
+
+def runapps():
+    for app in apps:
+        os.startfile(app)
+
 
 canvas = tk.Canvas(root, height=700, width=800, bg="blue")
 canvas.pack()
@@ -10,7 +31,7 @@ canvas.pack()
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(frame, text="Open Files", padx=10, pady=5, fg='white', bg='blue')
+openFile = tk.Button(frame, text="Open Files", padx=10, pady=5, fg='white', bg='blue', command=addApp)
 openFile.pack()
 
 runApp = tk.Button(frame, text="RunApps", padx=10, pady=5, fg='white', bg='blue')
