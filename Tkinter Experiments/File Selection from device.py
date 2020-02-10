c55bd72c94002efe2636fea00a2c4ef599af9bb8
devanshkaria88 <devansh88karia@gmail.com>
2020-02-10 12:52:08 +0530

    Explored all the methods for tkinter button

--- Tkinter Experiments/File Selection from device.py
+++ Tkinter Experiments/File Selection from device.py
@@ -7,16 +7,10 @@ apps = []
 
 
 def addApp():
-    for widget in frame.winfo_children():
-        widget.destroy()
-
     filename = filedialog.askopenfilename(initialdir='/', title='Select Files',
                                           filetypes=(('Exetuables', '*.exe'), ('All Files', '*.*')))
     apps.append(filename)
     print(filename)
-    for app in apps:
-        label = tk.Label(frame, text=app, bg='grey')
-        label.pack()
     runapps()
 
 
@@ -25,25 +19,13 @@ def runapps():
         os.startfile(app)
 
 
-canvas = tk.Canvas(root, height=700, width=800, bg="blue")
+canvas = tk.Canvas(root, height=300, width=300, bg="#274632")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-box1 = tk.Frame(frame, bg='red')
-box1.place(relwidth=0.5, relheight=0.5)
-
-box2 = tk.Frame(frame, bg='blue')
-box2.place(relwidth=0.5, relheight=0.5, rely=0.5)
-
-box3 = tk.Frame(frame, bg='green')
-box3.place(relwidth=0.5, relheight=0.5, relx=0.5)
-
-box4 = tk.Frame(frame, bg='yellow')
-box4.place(relwidth=0.5, relheight=0.5, relx=0.5, rely=0.5)
-
-openFile = tk.Button(root, text="Open Files", padx=10, pady=5, fg='white', bg='#2f2d2c', command=addApp)
+openFile = tk.Button(frame, text="Open Files", padx=10, pady=5, command=addApp)
 openFile.pack()
 
 root.mainloop()
