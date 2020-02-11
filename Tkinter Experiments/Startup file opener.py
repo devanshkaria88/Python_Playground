ba51eabf35690975a2b136c62d55e1fba5f5b1cc
devanshkaria88 <devansh88karia@gmail.com>
2020-02-11 23:32:03 +0530

    Solved Aspect ratio Error

--- Tkinter Experiments/Startup file opener.py
+++ Tkinter Experiments/Startup file opener.py
@@ -3,15 +3,16 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
-apps = []
+root.geometry("500x500")
+apps = ["C:\TURBOC3\Turbo C++\Turbo C++.exe"]
 
 
 def addApp():
-    filename = filedialog.askopenfilename(initialdir='/', title='Select Files',
-                                          filetypes=(('Exetuables', '*.exe'), ('All Files', '*.*')))
-    apps.append(filename)
-    print(filename)
     runapps()
+    # filename = filedialog.askopenfilename(initialdir='/', title='Select Files',
+    #                                       filetypes=(('Exetuables', '*.exe'), ('All Files', '*.*')))
+    # apps.append(filename)
+    # print(filename)
 
 
 def runapps():
@@ -19,13 +20,13 @@ def runapps():
         os.startfile(app)
 
 
-canvas = tk.Canvas(root, height=500, width=500, bg="#274632")
-canvas.pack()
+base1 = tk.Frame(root, width=500, height=500, bg="#274632")
+base1.place(relwidth=1.0, relheight=1.0)
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(frame, text="Open All programs", padx=10, pady=5, command=addApp)
-openFile.pack()
+openFile = tk.Button(root, text="Open Turbo C++", padx=10, pady=5, command=addApp)
+openFile.place(relx=0.45, rely=0.45)
 
 root.mainloop()
