5fd18c7e0a90c3a4a14b2859fe0ec74eeb108e2c
devanshkaria88 <devansh88karia@gmail.com>
2020-02-15 19:58:03 +0530

    Completed the OTP cipher Logic

--- Ciphers/OTP Cipher.py
+++ Ciphers/OTP Cipher.py
@@ -1,29 +1,27 @@
 import secrets
 
-
-def intake_string():  # Here the plain text is converted into the n bit Binary String
-    plain_text = str(input("Enter your message"))  # Here we take the Plain text
-    message_binary = ''.join(format(ord(i), 'b') for i in plain_text)
-    print(message_binary)
-    return message_binary
-
-
-n = len(intake_string())
-
-
-def key_generator(n):
-    temp1 = []
-    for j in range(0, n):
-        b = str(secrets.randbelow(2))
-        temp1.append(b)
-    key = ''.join(temp1)  # Here the random key of the length of the n bit binary string is generated
-    return key
-
-
-print(key_generator(n))
-
-# Key                                                               Message Text
-
-# 1000001101001101001001101000001011110010101000110              1000100110010111101101100001110111011100111101000
-# 0001010111101000111010000101110110101111011001000              1000100110010111101101100001110111011100111101000
-# 1001001100101010100100010001101011001011010100111
+plain_text = 'Devansh'  # Here we take the Plain text
+message_binary = ''.join(format(ord(i), 'b') for i in plain_text)
+temp1 = []
+for j in range(0, len(message_binary)):
+    b = str(secrets.randbelow(2))
+    temp1.append(b)
+key = ''.join(temp1)  # Here the random key of the length of the n bit binary string is generated
+temp2 = []
+for i in range(0, len(key)):
+    if message_binary[i] == key[i]:
+        temp2.append('0')
+    else:
+        temp2.append('1')
+cipher_text = ''.join(temp2)
+temp3 = []
+for i in range(0, len(key)):
+    if cipher_text[i] == key[i]:
+        temp3.append('0')
+    else:
+        temp3.append('1')
+recieved_text = ''.join(temp3)
+print(message_binary)
+print(key)
+print(cipher_text)
+print(recieved_text)
